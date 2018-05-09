feb5d9a45339156c408666c17338f9dc72dbc18f
Bryan Liu <bsliu17@gmail.com>
2018-05-09 11:36:54 -0700

    zsh

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 apt-get install -y zsh
+echo ">>> Installing oh my zsh <<<"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo ">>> Installing oh my zsh DONE <<<"
 
 
 
